5a4d9331b49fd083681a9a822114cbb525eed8dd
ruto0844 <spider0844@gmail.com>
2022-09-08 13:26:55 +0900

    プログラム作成

---
+++ sum.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def n_sum(n):
+    total = 0
+    for i in range(1,n+1):
+        total+=i
+    return total
+
+
+if __name__ == "__main__":
+    n = input()
+    print("input ",n)
+    print("total "n_sum(int(n)))
